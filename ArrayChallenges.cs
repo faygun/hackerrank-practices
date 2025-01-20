e2d9d945a3413a5fbe8c5b457155210da6166011
faygun <faruk.aygun@hadilive.com>
2025-01-20 14:45:08 +0000

    array problem solution added

--- ArrayChallenges.cs
+++ ArrayChallenges.cs
@@ -26,4 +26,27 @@ public static class ArrayChallenges
         }
         return sumList.Max();
     }
+
+    /*
+        https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+    */
+
+    public static List<int> rotLeft(List<int> a, int d)
+    {
+        var size = a.Count;
+        var res = new int[size];
+        if(size == d)
+            return a;
+        
+        
+        for(var i = 0; i < size; i++){
+            if(size + i - d >= size)
+                res[i - d] = a[i];
+            else
+                res[size + i - d] = a[i];
+        }
+        
+        return res.ToList();
+    }
+
 }
